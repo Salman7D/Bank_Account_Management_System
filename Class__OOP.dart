86da8c9b22da5fa560acc1e00e6882e292a27e78
Salman7D <salmanrayhan753@gmail.com>
2026-09-09 23:20:43 +0600

    constructor created

--- Class__OOP.dart
+++ Class__OOP.dart
@@ -1,8 +1,11 @@
 class BankAccount{
-  String ? accountHolderName;
-  int ? accountNumber;
-  int ? balance;
+  String accountHolderName;
+  int accountNumber;
+  int _balance;
+
+  BankAccount(this.accountHolderName, this.accountNumber, this._balance);
 }
+
 
 
 
