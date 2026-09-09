0fa5a1128e919e20da6dea64afb3509a2fefe09e
Salman7D <salmanrayhan753@gmail.com>
2026-09-09 23:39:51 +0600

    deposit method created

--- Class__OOP.dart
+++ Class__OOP.dart
@@ -1,13 +1,17 @@
-class BankAccount{
+class BankAccount {
   String accountHolderName;
   int accountNumber;
-  int _balance;
+  double _balance;
 
   BankAccount(this.accountHolderName, this.accountNumber, this._balance);
-}
 
 
+  void deposit(double amount) {
+    _balance += amount;
+  }
 
+
+    }
 
 
 
